f9eaf670fc4cc11292fc9ae7693a05368a09915b
Avtakhov <avtahovfarit@gmail.com>
2021-04-13 23:20:40 +0300

    fix infinity GradientMethod

--- core/src/com/mygdx/graphics/GraphicEngine.java
+++ core/src/com/mygdx/graphics/GraphicEngine.java
@@ -9,14 +9,14 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.*;
+import com.badlogic.gdx.utils.Align;
 import com.mygdx.graphics.parser.ExpressionParser;
 import com.mygdx.methods.*;
 import com.mygdx.nmethods.*;
-import com.mygdx.nmethods.Vector;
 
-import java.util.*;
-import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class GraphicEngine extends ApplicationAdapter {
     private Batch batch;
@@ -24,43 +24,53 @@ public class GraphicEngine extends ApplicationAdapter {
     private Stage stage;
     private Graphic graphic;
     private RenderFunction func;
-    private Input.TextInputListener input;
+    private Input.TextInputListener inputFunction;
+    private Input.TextInputListener inputEps;
     private ExpressionParser parser;
+    private double eps;
+
 
     private void inputFunction(final String title) {
-        Gdx.input.getTextInput(input, title, "10*x*x - x*y + y*y - 2*x + y + 8", "");
+        Gdx.input.getTextInput(inputFunction, title, "10*x*x - x*y + y*y - 2*x + y + 8", "");
+    }
+
+    private void inputEps(final String title) {
+        Gdx.input.getTextInput(inputEps, title, "1e-5", "");
     }
 
     private void callTest(RenderFunction func, Function<RenderFunction, AbstractNMethod<RenderFunction>> methodCreator) {
-        new DrawableNMethod(methodCreator.apply(func)).findMin(1e-2);
+        new DrawableNMethod(methodCreator.apply(func)).findMin(eps);
         graphic.setMain(func);
     }
 
-    private void addButton(
+    private Image addButton(final String name, final float x, final float y, Runnable runnable) {
+        Image b = new Image(new Texture("button_" + name + ".png"));
+        b.setPosition(x, y);
+        stage.addActor(b);
+        b.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                runnable.run();
+            }
+        });
+        return b;
+    }
+
+    private void addButtonMethod(
             final String name,
             final float x,
             final float y,
             final Function<RenderFunction, AbstractNMethod<RenderFunction>> methodCreator) {
-        Image b = new Image(new Texture("button_" + name + ".png"));
-        b.setPosition(x, y);
-        b.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                callTest(func, methodCreator);
-            }
-        });
+        Image b = addButton(name, x, y, () -> callTest(func, methodCreator));
         stage.addActor(b);
     }
 
-    @Override
-    public void create() {
-        parser = new ExpressionParser();
-        stage = new Stage();
-        batch = stage.getBatch();
-        input = new Input.TextInputListener() {
+    private void initInputs() {
+        inputFunction = new Input.TextInputListener() {
             @Override
             public void input(String text) {
                 func = new RenderFunction(parser.parse(text));
+                inputEps("Input epsilon");
             }
 
             @Override
@@ -70,27 +80,39 @@ public class GraphicEngine extends ApplicationAdapter {
                 }
             }
         };
+
+        inputEps = new Input.TextInputListener() {
+            @Override
+            public void input(String text) {
+                eps = Double.parseDouble(text);
+            }
+
+            @Override
+            public void canceled() {
+                inputEps("эй");
+            }
+        };
+    }
+
+    @Override
+    public void create() {
+        parser = new ExpressionParser();
+        stage = new Stage();
+        batch = stage.getBatch();
+        initInputs();
         inputFunction("Enter function, use +-*");
         graphicRenderer = new ShapeRenderer();
-        graphic = new Graphic(graphicRenderer);
-        graphic.setBounds(50, 50, 900, 900);
-        stage.addActor(graphic);
+        graphic = new Graphic(graphicRenderer, stage);
+        graphic.setSize(900, 900);
         InputMultiplexer inputMultiplexer = new InputMultiplexer();
         inputMultiplexer.addProcessor(graphic);
         inputMultiplexer.addProcessor(stage);
         Gdx.input.setInputProcessor(inputMultiplexer);
-        addButton("gradient-method", 1100, 800, GradientMethod::new);
-        addButton("optimized-gradient", 1100, 650, func -> new GradientOpt<>(func, BrentCombMethod::new));
-        addButton("conjugate-gradient", 1100, 500, NonlinearConjugateGradientMethod::new);
-        Image change = new Image(new Texture("button_change-function.png"));
-        change.setPosition(1100, 350);
-        change.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                Gdx.input.getTextInput(input, "Input function", "10*x*x - x*y + y*y - 2*x + y + 8", "");
-            }
-        });
-        stage.addActor(change);
+        addButtonMethod("gradient-method", 1100, 800, GradientMethod::new);
+        addButtonMethod("optimized-gradient", 1100, 650, func -> new GradientOpt<>(func, BrentCombMethod::new));
+        addButtonMethod("conjugate-gradient", 1100, 500, NonlinearConjugateGradientMethod::new);
+        addButton("change-function", 1100, 350, () -> inputFunction("Input function"));
+        addButton("level-lines-toggle", 1500, 350, () -> graphic.draw_levels = !graphic.draw_levels);
     }
 
     @Override
